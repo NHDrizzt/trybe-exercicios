68fb6123b0cc9d8a448ea75f9507b2f56f32f883
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-02-17 19:29:53 -0300

    encode and decode function tests

--- trybe-exercicios/fundamentos/secao-06-teste-unitario/dia-02-jest/script.js
+++ trybe-exercicios/fundamentos/secao-06-teste-unitario/dia-02-jest/script.js
@@ -1,35 +1,77 @@
 function myRemove(arr, item) {
+  let newArr = [];
 
-    let newArr = [];
-
-    for (let index = 0; index < arr.length; index += 1) {
-
-        if (item !== arr[index]) {
-
-            newArr.push(arr[index]);
-
-        }
-
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
     }
+  }
 
-    return newArr;
-
+  return newArr;
 }
 
 function myFizzBuzz(num) {
+  if (typeof num !== "number") return false;
 
-    if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
 
-    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return "fizz";
 
-    if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return "buzz";
 
-    if (num % 5 === 0) return 'buzz';
-
-    return num;
-
+  return num;
 }
 
-console.log(myFizzBuzz(3))
+const mapString = (objectMap, string) => {
+  const splitString = string.split("");
 
-module.exports = {myRemove, myFizzBuzz}
+  const mappedArray = [];
+
+  for (let index = 0; index < splitString.length; index += 1) {
+    const character = splitString[index];
+
+    const mappedValue = objectMap[character];
+
+    if (mappedValue) {
+      mappedArray.push(mappedValue);
+    } else {
+      mappedArray.push(character);
+    }
+  }
+
+  return mappedArray.join("");
+};
+
+const encode = (string) => {
+  const map = {
+    a: 1,
+
+    e: 2,
+
+    i: 3,
+
+    o: 4,
+
+    u: 5,
+  };
+
+  return mapString(map, string);
+};
+
+const decode = (string) => {
+  const map = {
+    1: "a",
+
+    2: "e",
+
+    3: "i",
+
+    4: "o",
+
+    5: "u",
+  };
+
+  return mapString(map, string);
+};
+
+module.exports = { myRemove, myFizzBuzz, encode, decode };
--- trybe-exercicios/fundamentos/secao-06-teste-unitario/dia-02-jest/script.test.js
+++ trybe-exercicios/fundamentos/secao-06-teste-unitario/dia-02-jest/script.test.js
@@ -1,31 +1,80 @@
-const { myRemove, myFizzBuzz } = require("./script");
+const { myRemove, myFizzBuzz, encode, decode } = require("./script");
 
-describe('myRemove tests', function () {
-    test('remove equal number inside array', () => {
-        expect(myRemove([1,2,3,4], 3)).toEqual([1,2,4])
-    })
-    test('should not return full array', () => {
-        expect(myRemove([1,2,3,4], 3)).not.toEqual([1,2,3,4])
-    })
-    test('should return full array', () => {
-        expect(myRemove([1,2,3,4], 5)).toEqual([1,2,3,4])
-    })
+describe("myRemove tests", function () {
+  test("remove equal number inside array", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  test("should not return full array", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test("should return full array", () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });
 
-describe('myFizzBuzz tests', () => {
-    test('check if num is divisible by 3 and 5', () => {
-        expect(myFizzBuzz(15)).toBe('fizzbuzz')
-    })
-    test('check if num is divisible by 3', () => {
-        expect(myFizzBuzz(3)).toBe('fizz')
-    })
-    test('check if num is divisible by 5', () => {
-        expect(myFizzBuzz(5)).toBe('buzz')
-    })
-    test('check if num is not divisible by 3 and 5', () => {
-        expect(myFizzBuzz(1)).toBe(1)
-    })
-    test('check if num is not divisible by 3 and 5', () => {
-        expect(myFizzBuzz('3')).toBe(false)
-    })
-})
+describe("myFizzBuzz tests", () => {
+  test("check if num is divisible by 3 and 5", () => {
+    expect(myFizzBuzz(15)).toBe("fizzbuzz");
+  });
+  test("check if num is divisible by 3", () => {
+    expect(myFizzBuzz(3)).toBe("fizz");
+  });
+  test("check if num is divisible by 5", () => {
+    expect(myFizzBuzz(5)).toBe("buzz");
+  });
+  test("check if num is not divisible by 3 and 5", () => {
+    expect(myFizzBuzz(1)).toBe(1);
+  });
+  test("check if num is not divisible by 3 and 5", () => {
+    expect(myFizzBuzz("3")).toBe(false);
+  });
+});
+
+describe("encode tests", () => {
+  test("should encode aeiou to 12345", () => {
+    expect(encode("aeiou")).toBe("12345");
+  });
+  it('converte apenas a vogal "a" no número 1', () => {
+    expect(encode("ana")).toEqual("1n1");
+  });
+  it('converte a vogal "e" no número 2', () => {
+    expect(encode("ele")).toEqual("2l2");
+  });
+  it('converte a vogal "i" no número 3', () => {
+    expect(encode("xixi")).toEqual("x3x3");
+  });
+  it('converte a vogal "o" no número 4', () => {
+    expect(encode("ovo")).toEqual("4v4");
+  });
+  it('converte a vogal "u" no número 5', () => {
+    expect(encode("nu")).toEqual("n5");
+  });
+});
+
+describe("decode tests", () => {
+  test("should decode 12345 to aeiou", () => {
+    expect(decode("12345")).toBe("aeiou");
+  });
+  it("converte apenas o número 1 na vogal a", () => {
+    expect(decode("1n1")).toEqual("ana");
+  });
+
+  it("converte apenas o número 2 na vogal e", () => {
+    expect(decode("2l2")).toEqual("ele");
+  });
+
+  it("converte apenas o número 3 na vogal i", () => {
+    expect(decode("x3x3")).toEqual("xixi");
+  });
+
+  it("converte apenas o número 4 na vogal o", () => {
+    expect(decode("4v4")).toEqual("ovo");
+  });
+
+  it("converte apenas o número 5 na vogal u", () => {
+    expect(decode("n5")).toEqual("nu");
+  });
+  test("decode function should return a value with same length as the string received", () => {
+    expect(decode("12345")).toHaveLength(5);
+  });
+});
